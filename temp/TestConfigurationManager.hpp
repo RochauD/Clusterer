04244d42be52e85e48475949799d7a02b8c589f2
Dinesh Acharya <d.acharya@jacobs-university.de>
2015-03-20 22:34:36 +0100

    Test for ConfigurationManager added. TestMain.cpp originally for testing ConfigurationReaderWriter was deleted and new files TestMainManager.cpp and TestMainReaderWriter.cpp were added.

---
+++ temp/TestConfigurationManager.hpp
@@ -0,0 +1,46 @@
+#ifndef TEST_BASIC_CONF_HPP
+#define TEST_BASIC_CONF_HPP
+
+#include <iostream>
+#include <fstream>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+///////////////////////////////////////////////////////
+#include <typeinfo>
+
+#include "ConfigurationManager.hpp"
+#include "ClusteringParams.hpp"
+//#include "ConfigurationReaderWriter.hpp"
+
+using namespace CppUnit;
+using namespace std;
+
+class TestConfigurationManager:public CppUnit::TestFixture{
+    CPPUNIT_TEST_SUITE(TestConfigurationManager);
+    CPPUNIT_TEST(testGetSetMethods);
+    CPPUNIT_TEST(testLoadClusteringParams);
+    CPPUNIT_TEST(testSaveClusteringParams);
+    CPPUNIT_TEST_SUITE_END();
+    
+public:
+    void setUp(void);
+    void tearDown(void);
+  
+protected:
+    void testGetSetMethods(void);
+    void testLoadClusteringParams(void);
+    void testSaveClusteringParams(void);
+  
+private:
+    ConfigurationManager *testConfManager;
+};
+
+#endif
