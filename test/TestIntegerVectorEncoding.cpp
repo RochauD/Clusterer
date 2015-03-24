86d7f03d697574e1dcbe733a0bb56722cd956f56
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2015-03-24 16:14:18 +0100

    Added empty test cases for IntegerVectorEncoding

---
+++ test/TestIntegerVectorEncoding.cpp
@@ -0,0 +1,14 @@
+#include "TestIntegerVectorEncoding.hpp"
+#include "../include/IntegerVectorEncoding.hpp"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( TestIntegerVectorEncoding );
+
+void TestIntegerVectorEncoding::setUp(void) {
+}
+
+void TestIntegerVectorEncoding::tearDown(void) {
+}
+
+void TestIntegerVectorEncoding::testFail(void) {
+    CPPUNIT_ASSERT(false);
+}
