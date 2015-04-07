d84139f293ecc697a64ef0aaf738c63f7f4e92f4
Kiril Kafadarov <k.kafadarov@jacobs-university.de>
2015-04-07 15:26:55 +0200

    not yet compiled initializer

--- src/IntegerEncodingInitializer.cpp
+++ src/IntegerEncodingInitializer.cpp
@@ -1,29 +1,39 @@
 #include "../include/IntegerVectorEncoding.hpp"
 
-namespace backend
+namespace clusterer
 {
-namespace genetic
+namespace backend
 {
 
 IntegerEncodingInitializer::IntegerEncodingInitializer(
         common::types::graph& g, unsigned maxClusters = 0) : graph(g)
 {
     if (maxClusters == 0 || maxClusters >= graph.getNoVertices())
-        maxClusters = graph.getNoVertices() - 1;
+    { maxClusters = graph.getNoVertices() - 1; }
 
     std::random_device rd;
     rng.seed(rd());
     uni_dist = new std::uniform_int_distribution<unsigned>(0, maxClusters);
 }
 
-IntegerVectorEncoding& IntegerEncodingInitializer::getRandomSolution()
+IntegerVectorEncoding IntegerEncodingInitializer::getRandomSolution()
 {
-    IntegerVectorEncoding result
+    IntegerVectorEncoding result(graph);
+    for (vert = 0; vert < graph.getNoVertices(); vert++)
+    { result.addToCluster(i, *uni_dist(rng)); }
+    return result;
 }
 
+std::vector<IntegerVectorEncoding> IntegerEncodingInitializer::getInitialPopulation(int count)
+{
+    std::vector<IntegerVectorEncoding> result;
+    for (i = 0; i < count; i++) 
+    { result.push_back(getRandomSolution()); }
+    return result;
+}
 
-}  // namespace genetic
 }  // namespace backend
+}  // namespace clusterer
 
 #endif  // _BACKEND_GENETIC_IntegerEncodingInitializer_HPP
 
