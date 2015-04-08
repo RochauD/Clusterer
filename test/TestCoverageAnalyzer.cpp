faac5034495220a93656dc299b3541c0b0ab1176
Rochau Denis <rochau.dev@gmail.com>
2015-04-08 15:30:56 +0200

    Added inital tests for coverage.

---
+++ test/TestCoverageAnalyzer.cpp
@@ -0,0 +1,133 @@
+#include "TestCoverageAnalyzer.hpp"
+
+#include <limits>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestCoverageAnalyzer);
+
+void TestCoverageAnalyzer::setUp(void)
+{
+
+}
+
+void TestCoverageAnalyzer::tearDown(void)
+{
+
+}
+
+void TestCoverageAnalyzer::testAnalyzeSingleElementClustersOnly(void)
+{
+    clb::Graph graph;
+
+    graph.addVertex(Vertex(0));
+    graph.addVertex(Vertex(1));
+    graph.addVertex(Vertex(2));
+    graph.addVertex(Vertex(3));
+
+    graph.addEdge(Vertex(0), Vertex(1), 0.125);
+    graph.addEdge(Vertex(0), Vertex(2), 0.125);
+    graph.addEdge(Vertex(0), Vertex(3), 0.125);
+
+    graph.addEdge(Vertex(1), Vertex(0), 0.25);
+    graph.addEdge(Vertex(1), Vertex(2), 0.25);
+    graph.addEdge(Vertex(1), Vertex(3), 0.25);
+
+    graph.addEdge(Vertex(2), Vertex(0), 0.5);
+    graph.addEdge(Vertex(2), Vertex(1), 0.5);
+    graph.addEdge(Vertex(2), Vertex(3), 0.5);
+
+    graph.addEdge(Vertex(3), Vertex(0), 0.75);
+    graph.addEdge(Vertex(3), Vertex(1), 0.75);
+    graph.addEdge(Vertex(3), Vertex(2), 0.75);
+
+    clb::IntegerVectorEncoding solution(graph);
+
+    solution.addToCluster(0, 0);
+    solution.addToCluster(1, 1);
+    solution.addToCluster(2, 2);
+    solution.addToCluster(3, 3);
+
+    double result = this->coverageAnalyzer.analyze(solution, graph);
+    if (result <  std::numeric_limits<double>::epsilon() && result > -std::numeric_limits<double>::epsilon())
+    {
+        result = 0.0;
+    }
+    CPPUNIT_ASSERT(result == 0.0);
+}
+
+void TestCoverageAnalyzer::testAnalyzeOneClusterOnly(void)
+{
+    clb::Graph graph;
+
+    graph.addVertex(Vertex(0));
+    graph.addVertex(Vertex(1));
+    graph.addVertex(Vertex(2));
+    graph.addVertex(Vertex(3));
+
+    graph.addEdge(Vertex(0), Vertex(1), 0.125);
+    graph.addEdge(Vertex(0), Vertex(2), 0.125);
+    graph.addEdge(Vertex(0), Vertex(3), 0.125);
+
+    graph.addEdge(Vertex(1), Vertex(0), 0.25);
+    graph.addEdge(Vertex(1), Vertex(2), 0.25);
+    graph.addEdge(Vertex(1), Vertex(3), 0.25);
+
+    graph.addEdge(Vertex(2), Vertex(0), 0.5);
+    graph.addEdge(Vertex(2), Vertex(1), 0.5);
+    graph.addEdge(Vertex(2), Vertex(3), 0.5);
+
+    graph.addEdge(Vertex(3), Vertex(0), 0.75);
+    graph.addEdge(Vertex(3), Vertex(1), 0.75);
+    graph.addEdge(Vertex(3), Vertex(2), 0.75);
+
+    clb::IntegerVectorEncoding solution(graph);
+
+    solution.addToCluster(0, 0);
+    solution.addToCluster(1, 0);
+    solution.addToCluster(2, 0);
+    solution.addToCluster(3, 0);
+
+    double result = this->coverageAnalyzer.analyze(solution, graph);
+    if (result <  1.0 + std::numeric_limits<double>::epsilon() && result > -1.0 - std::numeric_limits<double>::epsilon())
+    {
+        result = 1.0;
+    }
+    CPPUNIT_ASSERT(result == 1.0);
+}
+
+void TestCoverageAnalyzer::testAnalyzeFullyConnectedGraphTwoClusters(void)
+{
+    clb::Graph graph;
+
+    graph.addVertex(Vertex(0));
+    graph.addVertex(Vertex(1));
+    graph.addVertex(Vertex(2));
+    graph.addVertex(Vertex(3));
+
+    graph.addEdge(Vertex(0), Vertex(1), 0.125);
+    graph.addEdge(Vertex(0), Vertex(2), 0.125);
+    graph.addEdge(Vertex(0), Vertex(3), 0.125);
+
+    graph.addEdge(Vertex(1), Vertex(0), 0.25);
+    graph.addEdge(Vertex(1), Vertex(2), 0.25);
+    graph.addEdge(Vertex(1), Vertex(3), 0.25);
+
+    graph.addEdge(Vertex(2), Vertex(0), 0.5);
+    graph.addEdge(Vertex(2), Vertex(1), 0.5);
+    graph.addEdge(Vertex(2), Vertex(3), 0.5);
+
+    graph.addEdge(Vertex(3), Vertex(0), 0.75);
+    graph.addEdge(Vertex(3), Vertex(1), 0.75);
+    graph.addEdge(Vertex(3), Vertex(2), 0.75);
+
+    clb::IntegerVectorEncoding solution(graph);
+
+    solution.addToCluster(0, 0);
+    solution.addToCluster(1, 0);
+    solution.addToCluster(2, 1);
+    solution.addToCluster(3, 1);
+
+    double result = this->coverageAnalyzer.analyze(solution, graph);
+    double actualValue = (1.0/3.0); // @todo check with hand calculated one!!!
+    CPPUNIT_ASSERT((result <  actualValue + std::numeric_limits<double>::epsilon()) &&
+                   (result > -actualValue - std::numeric_limits<double>::epsilon()));
+}
---
+++ test/TestCoverageAnalyzer.hpp
@@ -0,0 +1,52 @@
+#ifndef TEST_BACKEND_COVERAGE_ANALYZER_HPP
+#define TEST_BACKEND_COVERAGE_ANALYZER_HPP
+
+// standard headers
+#include <typeinfo>
+// external headers
+#include <cppunit/TestCase.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+// internal headers
+#include "../include/CoverageAnalyzer.hpp"
+#include "../include/IntegerVectorEncoding.hpp"
+#include "../include/Graph.hpp"
+
+using namespace CppUnit;
+using namespace std;
+using namespace clb;
+
+class TestCoverageAnalyzer : public CppUnit::TestFixture
+{
+        CPPUNIT_TEST_SUITE(TestCoverageAnalyzer);
+        CPPUNIT_TEST(testAnalyzeSingleElementClustersOnly);
+        CPPUNIT_TEST(testAnalyzeOneClusterOnly);
+        CPPUNIT_TEST(testAnalyzeFullyConnectedGraphTwoClusters);
+        CPPUNIT_TEST_SUITE_END();
+
+    public:
+        void setUp(void);
+        void tearDown(void);
+
+    protected:
+        // each vertex in a seperate cluster
+        void testAnalyzeSingleElementClustersOnly(void);
+        // each vertex in same cluster
+        void testAnalyzeOneClusterOnly(void);
+        // each vertex in one of two clusters 2 in 0/ 2 in 1
+        void testAnalyzeFullyConnectedGraphTwoClusters(void);
+
+        void testAnalyzeFullyConnectedGraph(void);
+    private:
+        clb::CoverageAnalyzer coverageAnalyzer;
+};
+
+
+#endif
