c73c7636f96f9711cc3719f04011530ddded88ed
Dinesh Acharya <d.acharya@jacobs-university.de>
2015-04-13 22:49:46 +0200

    Test for Performance Analyzer

--- test/TestPerformanceAnalyzer.hpp
+++ test/TestPerformanceAnalyzer.hpp
@@ -40,11 +40,13 @@ class TestPerformanceAnalyzer : public CppUnit::TestFixture
         void testAnalyzeSingleElementClustersOnly(void);
         // each vertex in same cluster
         void testAnalyzeOneClusterOnly(void);
-        // each vertex in one of two clusters 2 in 0/ 2 in 1
-        void testAnalyzeFullyConnectedGraphTwoClusters(void);
+        // each vertex in one of two clusters
+        void testAnalyzeTwoClusters(void);
 
     private:
         clb::PerformanceAnalyzer performanceAnalyzer;
+	clb::Graph graph1;
+	clb::Graph graph2;
 };
 
 #endif
